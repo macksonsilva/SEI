eefe36c1653f605f319c63b4e11585a51914893f
Mackson <macksonsm@gmail.com>
2016-10-23 18:10:22 -0200

    Delete conexao.php

--- conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	try {
-		$con = new PDO("mysql:host=robb0378.publiccloud.com.br; dbname=atom_escola", "atom_escola", "manaus@#321");
-	} catch (Exception $e) {
-		echo $e->getMessage();
-	}
-
-?>
